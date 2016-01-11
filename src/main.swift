f3dd172d5affb14d41d469fcd009353879384f34
firestack <me@stackfire.io>
2016-01-11 02:45:33 -0700

    Working windowing and threaded event system

--- src/main.swift
+++ src/main.swift
@@ -2,86 +2,79 @@ import Foundation
 import Glibc
 import CSDL
 
-class OBJ{
 
+
+
+Engine.start()
+print(Engine.VERSION)
+
+Engine.createWindow(winSizeX:600, winSizeY:400)
+
+var GWindow = Engine.windows[0]
+var renderer = Engine.renderers[0]
+
+for i in 0..<10{
+	var a = Rect(Int32(i*2),Int32(i*2),2,2)
 }
 
+var EventHandler = {
+	var event = SDL_Event()
 
-protocol Renderable{
-	func render()
+	while(Engine.Running){
+		while(SDL_PollEvent(&event) != 0){
+			switch(event.type){
+				case SDL_QUIT.rawValue:
+					Engine.Running = false
 
-}
-
-public class Rect{
-	var rect = SDL_Rect()
-	var velocity = SDL_Rect()
-
-	init(_ x:Int32, _ y:Int32, _ w:Int32, _ h:Int32){
-		rect.x = x
-		rect.y = y
-		rect.w = w
-		rect.h = h
-		velocity.x = 1
-		velocity.y = 2
-	}
-	public func tick(screen:SDL_Rect){
-		translate(velocity.x, velocity.y)
-		if rect.x > screen.w || rect.x < 0{
-			velocity.x = -velocity.x
-		}
-
-		if rect.y > screen.h || rect.y < 0{
-			velocity.y = -velocity.y
+				default:
+					break
+			}
 		}
 	}
+}
 
-	public func render(inout renderer:SDL_Surface){
-		SDL_FillRect( &renderer, &rect, SDL_MapRGB(renderer.format, 255, 0, 0))
-	}
-
-	public func translate(x:Int32 = 0,_ y:Int32 = 0){
-		rect.x += x
-		rect.y += y
+var Mainloop = {
+	while(Engine.Running){
+		ControlProxy.gameLoop()
 	}
 }
 
-var SDLV:SDL_version = SDL_version()
+NSThread.detachNewThread({EventHandler()})
+Mainloop()
 
-SDL_GetVersion(&SDLV)
+Engine.stop()
 
-print("Hello world! \nSDL VERSION \(SDLV.major, SDLV.minor, SDLV.patch)")
-
-SDL_Init(UInt32(SDL_INIT_VIDEO))
-
-var GWindow = SDL_CreateWindow("SDL_T", 0, 0, 640, 480, SDL_WINDOW_SHOWN.rawValue)
-var GScreenSurface = SDL_GetWindowSurface( GWindow)
-var renderer = SDL_CreateRenderer(GWindow, -1, 0)
-
-print(GScreenSurface.memory, renderer)
-
-var running = true
-var MR = Rect(0,0,32,32)
-
-while(running){
-	var SDLEvent:SDL_Event = SDL_Event()
-	SDL_PollEvent(&SDLEvent)
-
-	switch (SDLEvent.type){
-		case SDL_QUIT.rawValue:
-			running = false
+//surfaces are out dated. lets use the renderer.
+//var GScreenSurface = SDL_GetWindowSurface( GWindow)
 
 
 
-		default:
-			MR.tick(GScreenSurface.memory.clip_rect)
-			MR.render(&GScreenSurface.memory)
-			SDL_UpdateWindowSurface(GWindow)
-			SDL_FillRect(GScreenSurface, nil, 0)
-			SDL_Delay(1000/30)
-	}
-
-
-}
-
-SDL_FreeSurface(GScreenSurface)
-SDL_DestroyWindow(GWindow)
+// while(running){
+// 	while(SDL_PollEvent(&SDLEvent) != 0){
+// 		switch (SDLEvent.type){
+// 			case SDL_QUIT.rawValue:
+// 				running = false
+//
+// 			case SDL_MOUSEBUTTONDOWN.rawValue:
+// 				MR.SetLocation(SDLEvent.button.x, SDLEvent.button.y)
+// 				MR.SetVelocity(0,0)
+// 				FLoc =  SDLEvent
+//
+// 			case SDL_MOUSEBUTTONUP.rawValue:
+// 				LLoc = SDLEvent
+//
+// 				MR.SetVelocity(LLoc.button.x - FLoc.button.x, LLoc.button.y - FLoc.button.y, dampening:10 )
+//
+//
+// 			default:
+// 				break
+//
+// 		}
+// 	}
+//
+// 	// MR.tick(GScreenSurface.memory.clip_rect)
+// 	// MR.render(&GScreenSurface.memory)
+// 	// SDL_UpdateWindowSurface(GWindow)
+// 	// SDL_FillRect(GScreenSurface, nil, 0)
+// 	// SDL_Delay(1000/120)
+// }
